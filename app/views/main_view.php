329cdead50089478e10f8ac0622cfa1479e35284
Purnama Anaking <purnama.anaking@gmail.com>
2022-03-31 15:55:00 +0700

    Update Placeholder

--- app/views/main_view.php
+++ app/views/main_view.php
@@ -43,7 +43,7 @@
 								<small>Silakan Ketik Topik Penelitian:</small>
 							</div>
 							<div class="input-group input-group-lg mb-4">
-								<input type="text" name="keyword" class="form-control" id="keyword" placeholder="IoT, Enterprise Architecture, Machine Learning, Database, ...">
+								<input type="text" name="keyword" class="form-control" id="keyword" placeholder="IoT Enterprise Architecture Machine Learning Database ...">
 								<span class="input-group-text bg-white">
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 										<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
