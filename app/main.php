5a3fd31f1f1a33544eca3834b31c98ff3f15cdcc
Purnama Anaking <purnama.anaking@gmail.com>
2022-05-10 10:47:07 +0700

    Show result by Department

--- app/main.php
+++ app/main.php
@@ -9,9 +9,22 @@
 			// Get Keyword
 			$search_keywords = $_POST['keyword'];
 
-			// Get Result
+			// Get Department
+			$departments_query = mysql_query("SELECT * FROM departments");
+			$departments = array();
+			while ($row = mysql_fetch_array($departments_query)) {
+				$departments[] = $row;
+			}
+
+			// Get All Data Result
 			$text_mining = new Text_mining();
-			$results = $text_mining->get_result($search_keywords);
+			$all_data_result = $text_mining->get_result($search_keywords);
+
+			// Get Result By Department
+			$by_department_result = array();
+			foreach($departments as $department) {
+				$by_department_result[$department['code']] = $text_mining->get_result($search_keywords, $department['code']);
+			}
 
 			// Setup Result View
 			include('views/result_view.php');
